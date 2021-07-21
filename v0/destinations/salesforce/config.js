5b01ca87810d41ed54a0b3021ac7d034512881e9
Chiradeep Vittal <chiradeep.vittal@lendistry.com>
2021-07-21 09:49:41 -0700

    Add SF contact owner id

--- v0/destinations/salesforce/config.js
+++ v0/destinations/salesforce/config.js
@@ -13,11 +13,15 @@ const SF_API_VERSION = "50.0";
 const SF_TOKEN_REQUEST_URL = process.env.SF_TOKEN_REQUEST_URL
   ?  process.env.SF_TOKEN_REQUEST_URL: "https://login.salesforce.com/services/oauth2/token";
 
+const SF_CONTACT_OWNER_ID = process.env.SF_CONTACT_OWNER_ID
+  ? process.env.SF_CONTACT_OWNER_ID:"00554000008kLjBAAU"; //API User
+
 const mappingConfig = getMappingConfig(ConfigCategory, __dirname);
 
 module.exports = {
   SF_API_VERSION,
   SF_TOKEN_REQUEST_URL,
+  SF_CONTACT_OWNER_ID,
   identifyMappingJson: mappingConfig[ConfigCategory.IDENTIFY.name],
   ignoredTraits: mappingConfig[ConfigCategory.IGNORE.name]
 };
